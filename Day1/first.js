be8aba19cd99b06d928d8a0917121d981507c079
Pushp Raj Bharti <pushprajbharti25@gmail.com>
2026-08-25 18:03:07 +0530

    Add all Git learning days

---
+++ Day1/first.js
@@ -0,0 +1,26 @@
+const name = "Pushp Raj Bharti";
+const age = 21;
+
+let balance = 4210;
+
+function updateBalance(value){
+    balance+=value;
+}
+
+
+console.log("Hello Coders");
+
+console.log("Welcome to JS World");
+
+
+function balancechecker(){
+    // 
+    console.log("Balance updated: " + balance);
+    // 
+}
+
+
+//  code likho
+//  5 member code likh rhe hai
+// update code: 
+// 3 line of code
---
+++ Day2/Git/first.js
@@ -0,0 +1,38 @@
+const name = "Pushp Raj Bharti";
+const age =19;
+const balance = 5000;
+
+console.log("Hello World");
+function updateBalance(){
+    balance+=1230;
+}
+
+updateBalance();
+console.log(balance);
+
+console.log("this is master branch");
+console.log("conflict arises");
+
+console.log("This is bug fixing.");
+
+function showAge(val){
+    console.log(age+ val);
+}
+showAge(10);
+
+
+
+function showName(){
+    console.log(name);
+}
+
+showName();
+
+
+function showName(){
+    console.log(name);
+}
+
+showName();
+
+
